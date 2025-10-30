36e87fd4bda5923f23a9d5db8be5fb1007cf3af8
Brainiac11 <saathvikkamma@gmail.com>
2025-10-30 14:43:17 +0000

    got the anle and knee and working on arm

--- turtlesim_ws/src/human_controller/human_controller/kick_controller.py
+++ turtlesim_ws/src/human_controller/human_controller/kick_controller.py
@@ -18,13 +18,15 @@ class KickController(Node):
         self.declare_parameter('hip_roll', 0.0)
         self.declare_parameter('hip_yaw_pitch', 0.0)
         self.declare_parameter('hip_pitch', 0.0)
+        self.declare_parameter('knee_pitch', 0.0)
+        self.declare_parameter('ankle_pitch', 0.0)
         self.declare_parameter('should_kick', 0.0)
 
-        self._knee_roll_subscription = self.create_subscription(Float64, '/LKneeRoll', self._hip_roll_callback, 1)
-        self._ankle_pitch_subscription = self.create_subscription(Float64, '/LAnklePitch', self._hip_yaw_pitch_callback, 1)
+        self._knee_pitch_subscription = self.create_subscription(Float64, '/LKneePitch', self._knee_pitch_callback, 1)
+        self._ankle_pitch_subscription = self.create_subscription(Float64, '/LAnklePitch', self._ankle_pitch_callback, 1)
         self._hip_pitch_subscription = self.create_subscription(Float64, '/LHipPitch', self._hip_pitch_callback, 1)
 
-        self._knee_roll_publisher = self.create_publisher(Float64, '/LKneeRoll', qos_profile=1)
+        self._knee_pitch_publisher = self.create_publisher(Float64, '/LKneePitch', qos_profile=1)
         self._ankle_pitch_publisher = self.create_publisher(Float64, '/LAnklePitch', qos_profile=1)
         self._hip_pitch_publisher = self.create_publisher(Float64, '/LHipPitch', qos_profile=1)
 
@@ -36,16 +38,20 @@ class KickController(Node):
         self._hip_roll = Float64(data=self.get_parameter("hip_roll").value)
         self._hip_yaw_pitch = Float64(data=self.get_parameter("hip_yaw_pitch").value)
         self._hip_pitch = Float64(data=self.get_parameter("hip_pitch").value)
+        self._knee_pitch = Float64(data=self.get_parameter("knee_pitch").value)
+        self._ankle_pitch = Float64(data=self.get_parameter("ankle_pitch").value)
         self._should_kick = KickCommand(data=self.get_parameter("should_kick").value)
 
         self.get_logger().info(f"Hip Roll: {self._hip_roll}")
     def _kick(self) -> None:
         if self._should_kick.should_kick:
             self._hip_pitch_controller(KickConstants.HIP_PITCH_KICK_ANGLE)
-            
+            self._ankle_pitch_controller(KickConstants.ANKLE_PITCH_KICK_ANGLE)
+            self._knee_pitch_controller(KickConstants.KNEE_PITCH_KICK_ANGLE)
         else:
             self._hip_pitch_controller(KickConstants.HIP_PITCH_START_ANGLE)
-            
+            self._ankle_pitch_controller(KickConstants.ANKLE_PITCH_START_ANGLE)
+            self._knee_pitch_controller(KickConstants.KNEE_PITCH_START_ANGLE)
 
     def _hip_roll_callback(self, roll: Float64) -> None:
         self._hip_roll = roll
@@ -56,14 +62,20 @@ class KickController(Node):
     def _hip_pitch_callback(self, pitch: Float64) -> None:
         self._hip_pitch = pitch
         print(self._hip_pitch)
+    def _knee_pitch_callback(self, pitch: Float64) -> None:
+        self._knee_pitch = pitch
+        print(self._knee_pitch)
+    def _ankle_pitch_callback(self, pitch: Float64) -> None:
+        self._ankle_pitch = pitch
+        print(self._ankle_pitch)
     def _should_kick_callback(self, should_kick: KickCommand) -> None:
         self._should_kick = should_kick
         print(self._should_kick)
-    def _hip_roll_controller(self, x: float) -> Float64:
-        self._knee_roll_publisher.publish(Float64(data=x))
+    def _knee_pitch_controller(self, x: float) -> Float64:
+        self._knee_pitch_publisher.publish(Float64(data=x))
         return Float64(data=x)
-    
-    def _hip_yaw_pitch_controller(self, x: float) -> Float64:
+
+    def _ankle_pitch_controller(self, x: float) -> Float64:
         self._ankle_pitch_publisher.publish(Float64(data=x))
         return Float64(data=x)
     def _hip_pitch_controller(self, x: float) -> Float64:
